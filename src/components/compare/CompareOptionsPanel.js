9a267fe5b123bd2b6a2b47103889300de71d6bc5
Jonathan <jonbalasin@gmail.com>
2021-11-23 15:22:38 +0000

    styling changes

--- src/components/compare/CompareOptionsPanel.js
+++ src/components/compare/CompareOptionsPanel.js
@@ -4,6 +4,10 @@ export function CompareOptionsPanel({}){
     return (
         <div id="compare-options-panel">
             <div className="panel-card">
+                <select name="work_days" id="id_work_days" multiple>
+                    <option value="1">Family</option>
+                    <option value="2">Crystal</option>
+                </select>
                 <input id="compare-search-query" type="text" placeholder="Crystal Name.."/>
                 <button id="compare-search-button">Search</button>
             </div>
